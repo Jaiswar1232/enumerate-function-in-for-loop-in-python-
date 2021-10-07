1bee262e43274b930773c20c7b4d511a8c600b94
Jaiswar1232 <87074705+Jaiswar1232@users.noreply.github.com>
2021-10-07 17:39:41 +0530

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -5,3 +5,10 @@ for index, element in enumerate(list_1):
    
  #Please note that, using range() function, to get index and accessing element using index, is not in the spirit of python. enumerate() is relatively recommended if you would like to access index in the for loop.  
 #enumerate() function keeps track of the count of elements we are iterating in the loop.
+
+# list of for loop practice link 
+#https://realpython.com/python-enumerate/ 
+#https://hsf-training.github.io/analysis-essentials/python/lists.html
+#https://www.programiz.com/python-programming/methods/built-in/slice
+#https://towardsdatascience.com/two-simple-ways-to-loop-more-effectively-in-python-886526008a70
+#https://www.dataquest.io/blog/python-range-tutorial/
