76aa5e25ba1f5024e934f9708c08ca0e14e54a73
Jaiswar1232 <87074705+Jaiswar1232@users.noreply.github.com>
2021-10-07 14:29:22 +0530

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -3,4 +3,5 @@ list_1 = ["apple", "banana", "orange", "mango"]
 for index, element in enumerate(list_1):
    print(index, element)
    
-   
+ #Please note that, using range() function, to get index and accessing element using index, is not in the spirit of python. enumerate() is relatively recommended if you would like to access index in the for loop.  
+#enumerate() function keeps track of the count of elements we are iterating in the loop.
